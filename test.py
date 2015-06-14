479279b3fbd8fe8a0d4d4f52557e7f700015d919
satyarth.mishra@gmail.com <satyarth.mishra@gmail.com>
2015-06-15 00:45:18 +0100

    Update test

--- test.py
+++ test.py
@@ -1,9 +1,27 @@
 from coordinate import Coordinate
 from geomaths import *
+from pygeocoder import Geocoder
+from pygeolib import GeocoderError
 
-a = Coordinate((45, 0))
-b = Coordinate((-45, 0))
-c = Coordinate((0, 90))
+cities = ['Abidjan', 'Manila', 'Auckland']
+coordinates = []
 
-print(circumcenters(a,c,b)[0].geo())
-print(distance(a,b))
+
+for city in cities:
+	print Geocoder.geocode(city).coordinates
+	coordinates.append(Coordinate(Geocoder.geocode(city).coordinates))
+
+
+cc = circumcenter(coordinates[0],coordinates[1],coordinates[2])
+
+print cc.geo(), antipode(cc).geo()
+print distance(coordinates[0], cc), distance(coordinates[0], antipode(cc))
+print Geocoder.reverse_geocode(cc.geo()[0], cc.geo()[1])
+
+# a = Coordinate((48.24,7.73))
+# b = Coordinate((66.38, 78))
+# c = Coordinate((30.5, 97.7))
+
+# cc = circumcenter(a,b,c)
+# print cc.geo(), antipode(cc).geo()
+# print distance(b, cc), distance(a, antipode(cc))
