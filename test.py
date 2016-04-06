2f1da90baa8e191532edbca7e35d617641715f48
Satyarth Mishra Sharma <mail@satyarth.me>
2016-04-06 17:28:44 +0300

    Python3

--- test.py
+++ test.py
@@ -8,15 +8,15 @@ coordinates = []
 
 
 for city in cities:
-	print Geocoder.geocode(city).coordinates
+	print(Geocoder.geocode(city).coordinates)
 	coordinates.append(Coordinate(Geocoder.geocode(city).coordinates))
 
 
 cc = circumcenter(coordinates[0],coordinates[1],coordinates[2])
 
-print cc.geo(), antipode(cc).geo()
-print distance(coordinates[0], cc), distance(coordinates[0], antipode(cc))
-print Geocoder.reverse_geocode(cc.geo()[0], cc.geo()[1])
+print(cc.geo(), antipode(cc).geo())
+print(distance(coordinates[0], cc), distance(coordinates[0], antipode(cc)))
+print(Geocoder.reverse_geocode(cc.geo()[0], cc.geo()[1]))
 
 # a = Coordinate((48.24,7.73))
 # b = Coordinate((66.38, 78))
