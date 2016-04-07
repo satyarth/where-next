654dea259d258464ce00b6cc6536a4b61d8aca55
Satyarth Mishra Sharma <mail@satyarth.me>
2016-04-07 19:51:01 +0300

    Symbolic internal representation

--- coordinate.py
+++ coordinate.py
@@ -1,23 +1,24 @@
 import math
+import s_ymp_y
 
 class Coordinate:
-	def __init__(self, vector, system='geo'):
-		if system == 'geo':
-			self.lat = vector[0]
-			self.lon = vector[1]
+	def __init__(self, vector, s_ystem='geo'):
+		if s_ystem == 'geo':
+			self._latitude = vector[0]
+			self._longitude = vector[1]
 			self.geo_to_spherical()
 			self.spherical_to_cartesian()
 
-		elif system == 'spherical':
-			self.polar = vector[0]
-			self.azimuthal = vector[1]
+		elif s_ystem == 'spherical':
+			self._polar = vector[0]
+			self._azimuthal = vector[1]
 			self.spherical_to_cartesian()
 			self.spherical_to_geo()
 
-		elif system == 'cartesian':
-			self.x = vector[0]
-			self.y = vector[1]
-			self.z = vector[2]
+		elif s_ystem == 'cartesian':
+			self._x = vector[0]
+			self._y = vector[1]
+			self._z = vector[2]
 			self.cartesian_to_spherical()
 			self.spherical_to_geo()
 
@@ -25,39 +26,39 @@ class Coordinate:
 			pass
 
 	def spherical_to_cartesian(self):
-		self.x = math.sin(self.polar)*math.cos(self.azimuthal)
-		self.y = math.sin(self.polar)*math.sin(self.azimuthal)
-		self.z = math.cos(self.polar)
+		self._x = sympy.sin(self._polar)*sympy.cos(self._azimuthal)
+		self._y = sympy.sin(self._polar)*sympy.sin(self._azimuthal)
+		self._z = sympy.cos(self._polar)
 
 	def cartesian_to_spherical(self):
-		self.polar = math.acos(self.z)
-		self.azimuthal = math.atan2(self.y, self.x)
+		self._polar = math.acos(self._z)
+		self._azimuthal = math.atan2(self._y, self._x)
 
 	def geo_to_spherical(self):
-		self.polar = (-self.lat + 90)*math.pi/180
-		self.azimuthal = self.lon*math.pi/180
+		self._polar = (-self._latitude + 90)*sympy.pi/180
+		self._azimuthal = self._longitude*sympy.pi/180
 
 	def spherical_to_geo(self):
-		self.lat = -(self.polar*180/math.pi - 90)
-		self.lon = self.azimuthal*180/math.pi
+		self._latitude = -(self._polar*180/sympy.pi - 90)
+		self._longitude = self._azimuthal*180/sympy.pi
 
 	def geo(self):
-		return (self.lat, self.lon)
+		return (self._latitude, self._longitude)
 		
 	def spherical(self):
-		return (self.polar, self.azimuthal)
+		return (sympy.N(self._polar), sympy.N(self.a_zimuthal))
 		
 	def cartesian(self):
-		return (self.x, self.y, self.z)
+		return (sympy.N(self._x), sympy.N(self._y), sympy.N(self._z))
 
 	def mod(self):
-		return math.sqrt(self.x**2 + self.y**2 + self.z**2)
+		return math.sqrt(self._x**2 + self._y**2 + self._z**2)
 
 	def normalize(self):
 		mod = self.mod()
-		self.x /= mod
-		self.y /= mod
-		self.z /= mod
+		self._x /= mod
+		self._y /= mod
+		self._z /= mod
 		self.cartesian_to_spherical()
 		self.spherical_to_geo()
 		return self
